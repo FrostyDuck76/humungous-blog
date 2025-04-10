1c05fc4127ffb695ea9bbfdd6ea08913c2729054
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-04-10 22:53:22 +1000

    Delete scripts/renew-certificate-dry-run.sh

--- scripts/renew-certificate-dry-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-echo "[$TIMESTAMP] User ran /usr/sbin/renew-certificate-dry-run.sh to test the renewal of a Let's Encrypt certificate" >> "/var/log/privileged_script_execution.log"
-
-sleep 5
-certbot renew --dry-run
