08773654917b848a1f2135f1b0c79ed08f77f81e
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-04-12 03:26:28 +1000

    Update my-startup-script.sh

--- scripts/my-startup-script.sh
+++ scripts/my-startup-script.sh
@@ -2,9 +2,16 @@
 
 # If the system is scheduled to regenerate an internal certificate, do it now
 if [ -f "/regenerate_internal_certificate.txt" ]; then
-    mv "/etc/ssl/private/internal.key" "/root/retired_certificates/"
-    mv "/etc/ssl/certs/internal.crt" "/root/retired_certificates/"
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/internal.key -out /etc/ssl/certs/internal.crt -subj "/O=Internal Services/OU=Infrastructure/CN=159.203.54.90" -addext "subjectAltName=IP:159.203.54.90"
+    TIME=$(printf '%(%Y_%m_%d_%H_%M_%S)T')
+    mkdir "/root/retired_certificates/$TIME/"
+    mv "/etc/ssl/private/internal.key" "/root/retired_certificates/$TIME/"
+    mv "/etc/ssl/certs/internal.crt" "/root/retired_certificates/$TIME/"
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/internal.key -out /etc/ssl/certs/internal.crt -subj "/O=Internal Services/OU=Infrastructure/CN=159.203.54.90" -addext "subjectAltName = IP:159.203.54.90"
+    chmod 600 "/etc/ssl/private/internal.key"
+    chmod 644 "/etc/ssl/certs/internal.crt"
+    restorecon "/etc/ssl/private/internal.key"
+    restorecon "/etc/ssl/certs/internal.crt"
+    rm "/regenerate_internal_certificate.txt"
 fi
 
 # Wait 3 minutes
