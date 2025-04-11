866110ae7005f2c8fc902973f704d0985c3013b7
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-04-11 21:03:32 +1000

    Update my-startup-script.sh

--- scripts/my-startup-script.sh
+++ scripts/my-startup-script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# If the system is scheduled to regenerate an internal certificate, do it now
+if [ -f "/regenerate_internal_certificate.txt" ]; then
+    mv "/etc/ssl/private/internal.key" "/root/retired_certificates/"
+    mv "/etc/ssl/certs/internal.crt" "/root/retired_certificates/"
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/internal.key -out /etc/ssl/certs/internal.crt -subj "/O=Internal Services/OU=Infrastructure/CN=159.203.54.90" -addext "subjectAltName=IP:159.203.54.90"
+fi
+
 # Wait 3 minutes
 sleep 180
 systemctl start telnet.socket
