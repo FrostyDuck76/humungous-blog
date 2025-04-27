1c3506254ed303271333f5331cf6a9fb9f57b2f3
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-04-28 03:19:41 +1000

    Update my-tcpdumpd.sh

--- scripts/my-tcpdumpd.sh
+++ scripts/my-tcpdumpd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export TIME=$(printf '%(%Y_%m_%d_%H_%M_%S)T')
+echo "capture_$TIME.pcap" > "/active_tcpdump_capture_file.txt"
 tcpdump --interface any -w "/var/log/tcpdumpd/capture_$TIME.pcap"
