3c1702ca8a3b54103c5c2928e7607a82331b5663
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-04-30 19:16:25 +1000

    Update my-tcpdumpd-killscript.sh

--- scripts/my-tcpdumpd-killscript.sh
+++ scripts/my-tcpdumpd-killscript.sh
@@ -6,7 +6,7 @@ kill -2 $TCPDUMP_PID
 
 # Temporarily unlock the AWS credentials from the user's home directory before uploading the most recent tcpdump capture file to an S3 bucket
 chown humungous:humungous "/home/humungous/.aws/credentials"
-su - -c "aws s3 cp /var/log/tcpdumpd/$(cat /active_tcpdump_capture_file.txt) s3://humungous-tcpdump-captures/" humungous
+su - -c "/usr/local/bin/aws s3 cp /var/log/tcpdumpd/$(cat /active_tcpdump_capture_file.txt) s3://humungous-tcpdump-captures/" humungous
 chown root:root "/home/humungous/.aws/credentials"
 
 rm "/active_tcpdump_capture_file.txt"
