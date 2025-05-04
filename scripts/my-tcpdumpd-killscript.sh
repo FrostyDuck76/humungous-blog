9516d7274803dc81ed13b79d1f32f7dcaf62ee13
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-05-05 02:03:09 +1000

    Update my-tcpdumpd-killscript.sh

--- scripts/my-tcpdumpd-killscript.sh
+++ scripts/my-tcpdumpd-killscript.sh
@@ -6,7 +6,12 @@ kill -2 $TCPDUMP_PID
 
 # Temporarily unlock the AWS credentials from the user's home directory before uploading the most recent tcpdump capture file to an S3 bucket
 chown humungous:humungous "/home/humungous/.aws/credentials"
-su - -c "/usr/local/bin/aws s3 cp /var/log/tcpdumpd/$(cat /active_tcpdump_capture_file.txt) s3://humungous-tcpdump-captures/" humungous
+chmod 644 "/s3_encryption_key.bin"
+gpg --batch --symmetric --cipher-algo AES256 --output "/home/humungous/$(cat /active_tcpdump_capture_file.txt).gpg" --passphrase-file "/s3_encryption_key.bin" "/var/log/tcpdumpd/$(cat /active_tcpdump_capture_file.txt)"
+chown humungous:humungous "/home/humungous/$(cat /active_tcpdump_capture_file.txt).gpg"
+su - -c "/usr/local/bin/aws s3 cp /home/humungous/$(cat /active_tcpdump_capture_file.txt).gpg s3://humungous-tcpdump-captures/" humungous
+rm "/home/humungous/$(cat /active_tcpdump_capture_file.txt).gpg"
+chmod 600 "/s3_encryption_key.bin"
 chown root:root "/home/humungous/.aws/credentials"
 
 rm "/active_tcpdump_capture_file.txt"
