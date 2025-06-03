8ac80d482339d95b6409e03ddf589b456757b033
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-06-04 03:26:31 +1000

    Update my-tcpdumpd-s3-uploader.sh

--- scripts/my-tcpdumpd-s3-uploader.sh
+++ scripts/my-tcpdumpd-s3-uploader.sh
@@ -3,12 +3,7 @@
 TCPDUMP_CAPTURE_FILE=$(cat /active_tcpdump_capture_file.txt)
 
 chown humungous:humungous "/home/humungous/.aws/credentials"
-chmod 644 "/s3_encryption_key.bin"
-gpg --batch --symmetric --cipher-algo AES256 --output "/home/humungous/$TCPDUMP_CAPTURE_FILE.gpg" --passphrase-file "/s3_encryption_key.bin" "/var/log/tcpdumpd/$TCPDUMP_CAPTURE_FILE"
-chown humungous:humungous "/home/humungous/$TCPDUMP_CAPTURE_FILE.gpg"
-su - -c "/usr/local/bin/aws s3 cp /home/humungous/$TCPDUMP_CAPTURE_FILE.gpg s3://humungous-tcpdump-captures/" humungous
-rm "/home/humungous/$TCPDUMP_CAPTURE_FILE.gpg"
-chmod 600 "/s3_encryption_key.bin"
+su - -c "/usr/local/bin/aws s3 cp /var/log/tcpdumpd/$TCPDUMP_CAPTURE_FILE s3://humungous-my-tcpdump-captures/web-app/" humungous
 chown root:root "/home/humungous/.aws/credentials"
 
 rm "/active_tcpdump_capture_file.txt"
