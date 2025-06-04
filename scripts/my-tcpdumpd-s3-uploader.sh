1e63b83666660b3cd830ee79b99baa148e6d4341
FrostyDuck76 <96914758+FrostyDuck76@users.noreply.github.com>
2025-06-05 01:08:56 +1000

    Update my-tcpdumpd-s3-uploader.sh

--- scripts/my-tcpdumpd-s3-uploader.sh
+++ scripts/my-tcpdumpd-s3-uploader.sh
@@ -3,7 +3,7 @@
 TCPDUMP_CAPTURE_FILE=$(cat /active_tcpdump_capture_file.txt)
 
 chown humungous:humungous "/home/humungous/.aws/credentials"
-su - -c "/usr/local/bin/aws s3 cp /var/log/tcpdumpd/$TCPDUMP_CAPTURE_FILE s3://humungous-my-tcpdump-captures/web-app/" humungous
+su - -c "/usr/local/bin/aws s3 cp /var/log/tcpdumpd/$TCPDUMP_CAPTURE_FILE s3://humungous-my-tcpdump-captures/web-app/i-0e1bcfb27a3fb8ceb/" humungous
 chown root:root "/home/humungous/.aws/credentials"
 
 rm "/active_tcpdump_capture_file.txt"
